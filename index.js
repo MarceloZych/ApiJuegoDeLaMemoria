14856f79c78531dec5d447bb8f3cc761a29d552b
MarceloZych <zych.omarcelo@gmail.com>
2024-07-11 20:10:34 -0300

    asd

--- index.js
+++ index.js
@@ -1,24 +1,37 @@
-const express = require('express');
-const axios = require('axios');
-const path = require('path');
-const app = express();
-const port = 3000;
+const express = require('express')
+const axios = require('axios')
+const path = require('path')
+const app = express()
+const port = 3000
+const pug = require('pug')
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 
+app.use(express.static('public'))
+
 app.get('/',async (req, res) => {
   try {
     const hpApi = await axios.get("https://hp-api.onrender.com/api/characters")
-    res.json(hpApi.data)
-    res.render('index', {title: 'Bienvenido', message: '¡Hola Mundo!', personajes: hpApi.data})
-    console.log(hpApi.data)
+    let pjConImg = hpApi.data.filter((pjImg) => pjImg.image != '')
+    let lista1 = pjConImg.slice(0,15)
+    let lista2 = pjConImg.slice(0,15)
+    let lista3 = lista1.concat(lista2)
+    lista3.sort(() => Math.random() - 0.5)
+    
+    lista3.forEach(element => {
+      element.estado = 'tapada'
+    })
+    lista3[0].estado = 'destapada'
+
+    console.log(lista3);
+    res.render('index', {title: 'Bienvenido', message: '¡Hola Mundo!', personajes: lista3})
   } catch (err) {
     console.error(err);
     res.status(500).json( {err : "Error al acceder a la api"})
   }
-});
+})
 
 app.listen(port, () => {
   console.log(`Servidor escuchando en el puerto ${port}`);
-});
+})
