583fb9934228ceeb61a5685593ebea23b748e793
MarceloZych <zych.omarcelo@gmail.com>
2024-07-19 15:47:01 -0300

    actualizado el seleccionar dos cartas 1

--- public/script.js
+++ public/script.js
@@ -1,37 +1,39 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const cards = document.querySelectorAll('.card')
-  let selectedCards = []
+  const cards = document.querySelectorAll('.card');
+  let selectedCards = [];
+  let lockBoard = false;
+
+  function checkForMatch(cards) {
+    const img1 = cards[0].querySelector('.card-img').getAttribute('data-image');
+    const img2 = cards[1].querySelector('.card-img').getAttribute('data-image');
+
+    if (img1 === img2) {
+      cards.forEach(card => card.querySelector('.card-img').setAttribute('data-status', 'destapada'));
+    } else {
+      cards.forEach(card => card.querySelector('.card-img').setAttribute('src', 'img/images.jpg'));
+    }
+  }
 
   cards.forEach(card => {
     card.addEventListener('click', () => {
+      if (lockBoard) return;
 
-      const img = card.querySelector('.card-img')
-      const imgSrc = img.getAttribute('data-image')
-
+      const img = card.querySelector('.card-img');
+      const imgSrc = img.getAttribute('data-image');
+      
       if (img.getAttribute('src') === 'img/images.jpg') {
-        img.setAttribute('src', imgSrc)
-        selectedCards.push(card)
+        img.setAttribute('src', imgSrc);
+        selectedCards.push(card);
       }
 
       if (selectedCards.length === 2) {
+        lockBoard = true;
         setTimeout(() => {
-          checkForMatch(selectedCards)
-          selectedCards = []
-        }, 1000)
+          checkForMatch(selectedCards);
+          selectedCards = [];
+          lockBoard = false;
+        }, 1000);
       }
-    })
-  })
-
-  const checkForMatch = (cards) => {
-    const img1 = cards[0].querySelector('.card-img').getAttribute('data-image')
-    const img2 = cards[1].querySelector('.card-img').getAttribute('data-image')
-
-    if (img1 === img2) {
-      cards[0].querySelector('.card-img').setAttribute('data-status', 'destapada')
-      cards[1].querySelector('.card-img').setAttribute('data-status', 'destapada')
-    } else {
-      cards[0].querySelector('.card-img').setAttribute('src', 'img/images.jpg')
-      cards[1].querySelector('.card-img').setAttribute('src', 'img/images.jpg')
-    }
-  }
-})
+    });
+  });
+});
